e24cd651917c004efe6632fdd53fdecbd76d9cbd
yujo <yujo.dev@gmail.com>
2021-10-11 17:43:12 +0900

    chore: init

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+const App = `<div class="container">
+              <span class="count">0</span>
+              <div class="btn-group">
+                <button>
+                  <strong>-</strong>
+                </button>
+                <button>
+                  <strong>RESET</strong>
+                </button>
+                <button>
+                  <strong>+</strong>
+                </button>
+              </div>
+            </div>`;
+
+export default App;
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import App from "app";
+
+const $root = document.querySelector("#root");
+
+$root!.innerHTML = App;
